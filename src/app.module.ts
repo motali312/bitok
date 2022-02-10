8663a657199760853b02aa26123ab52c1cdb7edd
Aikanysh <you@example.com>
2022-02-10 13:51:52 +0600

    fixed some errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { BlogModule } from './bit/bit.module';
+import { BitModule } from './bit/bit.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/bit-ly', { useNewUrlParser: true }),
-    BlogModule,
+    BitModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/bit/bit.module.ts
+++ src/bit/bit.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BitService } from './bit.service';
 import { BitController } from './bit.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BitEntity, BitSchema } from './entity';
 
 @Module({
+  imports:[
+    MongooseModule.forFeature([
+      {name:BitEntity.name,schema:BitSchema},
+    ])
+  ],
   providers: [BitService],
   controllers: [BitController]
 })
-export class BlogModule {}
+export class BitModule {}
